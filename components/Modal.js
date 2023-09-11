5d33d76f10bb047548843b9d8e67158ad71a3fce
Mkdenno <denniskiprop25@gmail.com>
2023-09-11 08:25:24 +0300

    It will add Modal component

--- components/Modal.js
+++ components/Modal.js
@@ -1,54 +1,54 @@
-// import { useRouter } from 'next/navigation'
-// import React, { useState } from 'react'
-// import { v4 as uuidv4 } from 'uuid';
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 
-// import {useDispatch} from 'react-redux'
-// import { addTodo } from '../redux/todoSlice';
-
-
-
-// const Modal= ({modalOpen,setModalOpen}) => {
-// 	const [value, setValue] = useState('');
+import {useDispatch} from 'react-redux'
+import { addTodo } from '../redux/todoSlice';
 
 
-//     const dispatch=useDispatch()
 
-// 	const onSubmit = (event) => {
-// 		event.preventDefault();
+const Modal= ({modalOpen,setModalOpen}) => {
+	const [value, setValue] = useState('');
 
 
-//         dispatch(addTodo({
-//           id:uuidv4(),
-//             title:value
-//         }))
-//         setModalOpen(false)
-//         setValue("")
-// 	};
+    const dispatch=useDispatch()
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+
+
+        dispatch(addTodo({
+          id:uuidv4(),
+            title:value
+        }))
+        setModalOpen(false)
+        setValue("")
+	};
     
-//   return (
-// <dialog id="my_modal_3 " className={`modal ${modalOpen ? "modal-open" : ""}`}>
-//   <div className="modal-box ">
-//     <form className='' method="dialog" onSubmit={onSubmit}>
-//       <button onClick={()=>setModalOpen(false)} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-//       <h3 className='bold text-lg'>Add new task</h3>
-//       <div className='modal-action'>
-//       <input
-//     type='text'
-//     className='form-control mb-2 mr-sm-2'
-//     placeholder='Add todo...'
-//     value={value}
-//     onChange={(event) => setValue(event.target.value)}></input>
+  return (
+<dialog id="my_modal_3 " className={`modal ${modalOpen ? "modal-open" : ""}`}>
+  <div className="modal-box ">
+    <form className='' method="dialog" onSubmit={onSubmit}>
+      <button onClick={()=>setModalOpen(false)} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+      <h3 className='bold text-lg'>Add new task</h3>
+      <div className='modal-action'>
+      <input
+    type='text'
+    className='form-control mb-2 mr-sm-2'
+    placeholder='Add todo...'
+    value={value}
+    onChange={(event) => setValue(event.target.value)}></input>
 
-// <button type='submit' className='btn btn-primary mb-2'>
-//     Submit
-// </button>
-//       </div>
-//     </form>
-//   </div>
-// </dialog>
-//   )
-// }
+<button type='submit' className='btn btn-primary mb-2'>
+    Submit
+</button>
+      </div>
+    </form>
+  </div>
+</dialog>
+  )
+}
 
 
 
-// export default Modal
+export default Modal
--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,46 +1,46 @@
 
-// 'use client'
-// import React from 'react';
-// import {useDispatch} from 'react-redux'
-// import { toggleComplete,deleteTodo } from '@/redux/todoSlice';
-// import { FiTrash2 } from "react-icons/fi";
+'use client'
+import React from 'react';
+import {useDispatch} from 'react-redux'
+import { toggleComplete,deleteTodo } from '@/redux/todoSlice';
+import { FiTrash2 } from "react-icons/fi";
 
 
-// const TodoItem = ({ id, title, completed }) => {
-//     const dispatch=useDispatch();
+const TodoItem = ({ id, title, completed }) => {
+    const dispatch=useDispatch();
 
-//     const handleDeleteTodo=()=>{
-//         dispatch(deleteTodo(
-//         id
-//         ))
-//     }
+    const handleDeleteTodo=()=>{
+        dispatch(deleteTodo(
+        id
+        ))
+    }
 
-//     const handlecompleteClick=()=>{
-//         dispatch(toggleComplete({
-//             id: id,
-//             completed: !completed
-//         }))
-//     }
-// 	return (
-
-
-//         <div className="overflow-x-auto">
-//   <table className="table">
-
-//     <tbody>
-//       <tr className="m-4 bg-base-200">
-//         <td className=''><input type='checkbox' className='mr-3' onChange={handlecompleteClick}></input></td>
-//         <td>{title}</td>
-//         <td><button onClick={handleDeleteTodo}> <FiTrash2 cursor="pointer" className="text-red-500"  /></button></td>
-//       </tr>
+    const handlecompleteClick=()=>{
+        dispatch(toggleComplete({
+            id: id,
+            completed: !completed
+        }))
+    }
+	return (
 
 
-//     </tbody>
-//   </table>
-// </div>
+        <div className="overflow-x-auto">
+  <table className="table">
+
+    <tbody>
+      <tr className="m-4 bg-base-200">
+        <td className=''><input type='checkbox' className='mr-3' onChange={handlecompleteClick}></input></td>
+        <td>{title}</td>
+        <td><button onClick={handleDeleteTodo}> <FiTrash2 cursor="pointer" className="text-red-500"  /></button></td>
+      </tr>
+
+
+    </tbody>
+  </table>
+</div>
 
         
-// 	);
-// };
+	);
+};
 
-// export default TodoItem;
+export default TodoItem;
