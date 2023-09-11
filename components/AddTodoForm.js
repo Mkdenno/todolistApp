bbe4875455e3a748931b23841d9d836b6db8e9c0
Mkdenno <denniskiprop25@gmail.com>
2023-09-11 08:26:35 +0300

    It will add AddTodo component

--- components/AddTodoForm.js
+++ components/AddTodoForm.js
@@ -1,21 +1,21 @@
-// 'use client'
-// import React, { useState } from 'react';
-// import {AiOutlinePlus} from 'react-icons/ai';
-// import Modal from './Modal';
+'use client'
+import React, { useState } from 'react';
+import {AiOutlinePlus} from 'react-icons/ai';
+import Modal from './Modal';
 
 
 
-// const AddTodoForm = () => {
-//     const [modalOpen, setModalOpen]=useState(false);
+const AddTodoForm = () => {
+    const [modalOpen, setModalOpen]=useState(false);
 
 
-// 	return (
+	return (
 
-//         <div>
-//             <button onClick={()=>setModalOpen(true)} className='btn btn-primary w-full'>Add New Task <AiOutlinePlus className='ml-2' size={10}/></button>
-//             <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}/>
-//        </div>
-// 	);
-// };
+        <div>
+            <button onClick={()=>setModalOpen(true)} className='btn btn-primary w-full'>Add New Task <AiOutlinePlus className='ml-2' size={10}/></button>
+            <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}/>
+       </div>
+	);
+};
 
-// export default AddTodoForm;
+export default AddTodoForm;
