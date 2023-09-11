b23a28b31e0505c29bafdaa23908c862694b0711
Mkdenno <denniskiprop25@gmail.com>
2023-09-11 08:26:06 +0300

    It will add TodoList component

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,19 +1,19 @@
-// 'use client'
-// import React from 'react';
-// import TodoItem from './TodoItem';
-// import {useSelector} from 'react-redux'
+'use client'
+import React from 'react';
+import TodoItem from './TodoItem';
+import {useSelector} from 'react-redux'
 
-// const TodoList = () => {
-//     const todos=useSelector((state)=>state.todos.todoList)
+const TodoList = () => {
+    const todos=useSelector((state)=>state.todos.todoList)
 
 
-// 	return (
-// 		<ul className='list-group'>
-// 			{todos.map((todo) => (
-// 				<TodoItem id={todo.id} title={todo.title} completed={todo.completed} />
-// 			))}
-// 		</ul>
-// 	);
-// };
+	return (
+		<ul className='list-group'>
+			{todos.map((todo) => (
+				<TodoItem id={todo.id} title={todo.title} completed={todo.completed} />
+			))}
+		</ul>
+	);
+};
 
-// export default TodoList;
+export default TodoList;
